5f8a4e8e4a659b15f8cf9476d0a147255ce2b3f2
Raniket <raniketram@gmail.com>
2019-05-08 16:35:31 +0530

    app dockarization

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 9000;
+const HOST = '0.0.0.0';
 
-server.listen(PORT, () => {
+server.listen(PORT, HOST, () => {
     console.log(`Server is running on port :  ${PORT}`);
 });
